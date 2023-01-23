bb948156654cebdca1f803d4fb3c0b1215cf1dde
lil-dev16 <olusegunkd2017@gmail.com>
2023-01-23 22:54:24 +0100

    urls

--- src/context.jsx
+++ src/context.jsx
@@ -2,18 +2,20 @@ import { createContext, useContext, useEffect } from "react"
 
 const AppContext = createContext();
 
+    const allMeasUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
+    const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 const ContextProvider = ({children}) => {
+    const fetchfunc = async () => {
+        try{
+            const res = await fetch('https://randomuser.me/api')
+            const out = await res.json() 
+            console.log(out);
+        }catch{
+            console.log('err err, aliens attacked');
+        }
+    }
     useEffect(()=> {
         console.log('fetch data here');
-        const fetchfunc = async () => {
-            try{
-                const res = await fetch('https://randomuser.me/api')
-                const out = await res.json() 
-                console.log(out);
-            }catch{
-                console.log('err err, aliens attacked');
-            }
-        }
         fetchfunc()
     }, [])
     return <AppContext.Provider value={{name: 'Segun', age: 15}}>
