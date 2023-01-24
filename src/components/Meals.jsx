c9cbbfcb56ad6ad92a724acc877a8a514db9ee05
lil-dev16 <olusegunkd2017@gmail.com>
2023-01-24 19:37:33 +0100

    feature: tailwind

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -8,9 +8,11 @@ const Meals = () => {
     <section>
       {
         meals.map(({idMeal, strMeal, strMealThumb}) => {
-          return <div key={idMeal}>
+          return <div key={idMeal} className="meal">
             <img src={strMealThumb} alt={strMeal} />
+            <div className='meal-text'>
             <h1>{strMeal}</h1>
+            </div>
           </div>
         })
       }
