1de11786b5e74df969757f03ac82814c578a49ab
lil-dev16 <olusegunkd2017@gmail.com>
2023-01-24 22:03:53 +0100

    feature: loading

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 
 const Meals = () => {
-    const {meals} = useGlobalContext();
+    const {meals, loading} = useGlobalContext();
 
   return (
     <section className='grid-cols-3 grid gap-[2rem]'>
       {
+        loading ? <div>Loading</div> :
         meals.map(({idMeal, strMeal, strMealThumb}) => {
           return <div key={idMeal} className="meal w-[350px] border">
             <img src={strMealThumb} alt={strMeal} />
--- src/context.jsx
+++ src/context.jsx
@@ -3,24 +3,28 @@ import axios from "axios";
 
 const AppContext = createContext();
 
-    const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+    const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=n';
     const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 const ContextProvider = ({children}) => {
     const [meals, setMeals] = useState([]);
+    const [loading, setLoading] = useState(false);
     const fetchMeals = async (url) => {
+        setLoading(true)
         try{
             const {data} = await axios(url)
             setMeals(data.meals)
             console.log(data);
+           
         }catch{
             console.log('err err, aliens attacked');
         }
+        setLoading(false)
     }
     useEffect(()=> {
         console.log('fetch data here');
         fetchMeals(allMealsUrl)
     }, [])
-    return <AppContext.Provider value={{meals}}>
+    return <AppContext.Provider value={{meals, loading}}>
         {children}
     </AppContext.Provider>
 }
