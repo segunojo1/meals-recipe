f3fd85dcf3500abb76e0e1a2ffb2bca09b532a63
lil-dev16 <olusegunkd2017@gmail.com>
2023-01-29 17:42:08 +0100

    mod

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,12 +6,13 @@ const Modal = () => {
   const close = () => {
     setShowModal(false)
   }
-  const {idMeal, strMeal} = selectedMeal
+  const {idMeal, strMeal,strMealThumb} = selectedMeal
   return (
     <div className='w-full h-[100vh] bg-[green] fixed top-0 flex'>
       <div className='w-[70%] h-[50%] bg-[red] m-auto rounded-lg' >
-        <img src="" alt="" />
+        <img src={strMealThumb} alt="" />
         <h1>{strMeal}</h1>
+        <p></p>
         <button className='p-[1rem]' onClick={close}>close</button>
       </div>
     </div>
